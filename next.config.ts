a487b4da18bfec2724c0b24f35bb5fda01681711
sam <samridhivigd.a.v@gmail.com>
2026-09-11 00:00:47 +0200

    Exclude non-Linux ONNX binaries in outputFileTracingExcludes for Vercel

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from 'next';
 
 const config: NextConfig = {
   serverExternalPackages: ['@huggingface/transformers', 'onnxruntime-node', 'onnxruntime-web', 'pdf-parse'],
+  outputFileTracingExcludes: {
+    '*': [
+      './node_modules/onnxruntime-node/bin/napi-v6/win32/**',
+      './node_modules/onnxruntime-node/bin/napi-v6/darwin/**',
+      './node_modules/onnxruntime-node/bin/napi-v6/linux/arm64/**',
+      './node_modules/onnxruntime-node/bin/napi-v6/linux/arm/**',
+      'node_modules/onnxruntime-node/bin/napi-v6/win32/**',
+      'node_modules/onnxruntime-node/bin/napi-v6/darwin/**',
+      'node_modules/onnxruntime-node/bin/napi-v6/linux/arm64/**',
+      'node_modules/onnxruntime-node/bin/napi-v6/linux/arm/**',
+    ],
+  },
 };
 
 export default config;
