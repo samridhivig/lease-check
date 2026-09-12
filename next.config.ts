5ebb9f131447615231f2a4992457ae6d0f51e9a9
sam <samridhivigd.a.v@gmail.com>
2026-09-12 13:19:30 +0200

    Remove onnxruntime-web from serverExternalPackages and add to outputFileTracingExcludes

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,33 @@
 import type { NextConfig } from 'next';
 
 const config: NextConfig = {
-  serverExternalPackages: ['@huggingface/transformers', 'onnxruntime-node', 'onnxruntime-web', 'pdf-parse'],
+  serverExternalPackages: ['@huggingface/transformers', 'onnxruntime-node', 'pdf-parse'],
   outputFileTracingExcludes: {
+    '/api/translate': [
+      './node_modules/onnxruntime-node/bin/napi-v6/win32/**',
+      './node_modules/onnxruntime-node/bin/napi-v6/darwin/**',
+      './node_modules/onnxruntime-node/bin/napi-v6/linux/arm64/**',
+      './node_modules/onnxruntime-node/bin/napi-v6/linux/arm/**',
+      './node_modules/onnxruntime-web/**',
+      'node_modules/onnxruntime-node/bin/napi-v6/win32/**',
+      'node_modules/onnxruntime-node/bin/napi-v6/darwin/**',
+      'node_modules/onnxruntime-node/bin/napi-v6/linux/arm64/**',
+      'node_modules/onnxruntime-node/bin/napi-v6/linux/arm/**',
+      'node_modules/onnxruntime-web/**',
+      'node_modules/@huggingface/transformers/.cache/**',
+    ],
     '*': [
       './node_modules/onnxruntime-node/bin/napi-v6/win32/**',
       './node_modules/onnxruntime-node/bin/napi-v6/darwin/**',
       './node_modules/onnxruntime-node/bin/napi-v6/linux/arm64/**',
       './node_modules/onnxruntime-node/bin/napi-v6/linux/arm/**',
+      './node_modules/onnxruntime-web/**',
       'node_modules/onnxruntime-node/bin/napi-v6/win32/**',
       'node_modules/onnxruntime-node/bin/napi-v6/darwin/**',
       'node_modules/onnxruntime-node/bin/napi-v6/linux/arm64/**',
       'node_modules/onnxruntime-node/bin/napi-v6/linux/arm/**',
+      'node_modules/onnxruntime-web/**',
+      'node_modules/@huggingface/transformers/.cache/**',
     ],
   },
 };
